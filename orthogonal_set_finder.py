4db2c1ffe933cdd596c47e01cd13a4128130ba1c
c-bun <colrath@gmail.com>
2016-11-21 09:56:39 -0800

    Initial Commit

---
+++ orthogonal_set_finder.py
@@ -0,0 +1,151 @@
+#!/usr/bin/python
+"""
+Created on Thu Oct 27 2016
+
+@author: colinrathbun
+
+Script file to contain all the moving parts for a multithreaded orthogonal set search to be used on the computational cluster or by other users.
+"""
+
+# imports
+import pandas as pd
+import numpy as np
+import matplotlib
+import itertools
+import threading
+from itertools import chain
+
+# setup for plotting. for some reason this does not work right now.
+# matplotlib.style.use('fivethirtyeight')
+
+def norm_positives(l):
+    """
+    Helper function for the smooth_mat function to allow normalization of only positive values.
+    """
+    p = l.copy()
+    p[p<0] = 0
+    total = sum(p)
+    return l/total
+
+def smooth_mat(unmixed, remove_negatives=False):
+    """
+    Takes and unmixed array of correlations and normalizes rows(?) to one, ignoring negative values. remove_negatives will zero the negative values in the row(?).
+    """
+    if remove_negatives:
+        unmixed[unmixed < 0] = 0
+    return unmixed.apply(norm_positives, axis=1)
+
+def clean_raw_data(pdarray):
+    """
+    Modifies pdarray in place to set any value below 1E3 to 1E3.
+    """
+    for flux_value in np.nditer(pdarray, op_flags=['readwrite']):
+        if flux_value < 1000:
+            flux_value[...] = 1000 #using the elipsis will actually set the value in the array
+
+def trim_data(data, support):
+    """
+    Looks like this will trim a data set to only have the columns of a second set. May still be useful...?
+    """
+    chosen = dict(zip(data.columns, support))
+    chosenList = []
+    for entry in chosen:
+        if chosen[entry] == True:
+            chosenList.append(entry)
+    newData = data[chosenList].copy()
+    return newData
+
+def every_matrix(m, n, pandasArray):
+    """
+    Accepts a pandas dataframe and returns an iterator with every possible combination of m rows and n columns via an iterator object.
+    """
+    index_comb = itertools.combinations(pandasArray.index, m)
+    column_comb = itertools.combinations(pandasArray.columns, n)
+    index_column_prod = itertools.product(index_comb, column_comb)
+    return index_column_prod
+
+def get_submatrix(full_data, combination_tuple):
+    """
+    Accepts a tuple from the every_matrix() iterator to return the actual submatrix of the full data (not a copy).
+    """
+    return full_data[list(combination_tuple[1])].loc[list(combination_tuple[0])]
+
+def RMS_identity(pandasArray):
+    """
+    Returns the average RMS error of the given matrix from the identity matrix.
+    """
+    return np.sqrt(((pandasArray - np.eye(pandasArray.shape[0])) ** 2).values.mean(axis=None))
+
+def avg_off_diag_value(npArray):
+    np.fill_diagonal(npArray, 0)
+    return npArray.mean(axis=None)
+
+def normalize_vectors(pandasArray):
+    return pandasArray/np.linalg.norm(pandasArray, axis=0)
+
+def remove_dim_bands(full_data, threshold):
+    """
+    Still need to decide how to implement this. Would potentially filter out low-emitting sets.
+    """
+    trimmed_dataframe = full_data.copy()
+    for column in full_data:
+        if np.max(full_data[column]) < threshold:
+            trimmed_dataframe.drop(column)
+    return trimmed_dataframe
+
+def check_RMSs(submatrix_indicies, full_data):
+    '''
+    Takes a tuple of the required indicies and the full matrix of data.
+    Gets the rms and returns tuple (<RMS>, <DataFrame>).
+    '''
+    submatrix = get_submatrix(full_data, submatrix_indicies)
+    submatrix_normd = normalize_vectors(submatrix)
+    orthog_submatrix = submatrix_normd.dot(submatrix_normd.T)
+    result = (RMS_identity(orthog_submatrix), submatrix)
+    return result
+
+def iterate_RMSs(list_to_process, full_data):
+    '''
+    Takes a list of tuples of columns and rows to process and the full data
+    matrix and iterates through the list, returning the RMS rating and the
+    associated matrix.
+    '''
+    result_list = []
+    for combination in list_to_process:
+        result_list.append(check_RMSs(combination, full_data)) #begin checking RMSs here.
+    return result_list
+
+def run_singleprocess(full_data, dimension):
+    '''
+    Method to run OSF search in one processes for testing. Compounds must be in
+    the rows of full_data for this method (not the case for run_multiprocess()
+    in run_OSF.py.
+    '''
+    combinations = every_matrix(dimension,dimension,full_data)
+    result_list = iterate_RMSs(combinations, full_data)
+    return sorted(result_list, key=lambda x: x[0])
+
+# When to_csv used, prints compounds in quotes and mutants as floats, this is
+# a bit annoying.
+def format_OSF(sorted_result_list, list_len=100):
+    '''
+    Takes a result list from run_singleprocess() or run_multiprocess() and
+    formats a DataFrame for export with DataFrame.to_csv().
+    '''
+    working_list = []
+    for i in range(list_len):
+        working_list.append([
+            i+1,
+            sorted_result_list[i][0],
+            ', '.join(map(str,sorted(sorted_result_list[i][1].index))),
+            ', '.join(map(str,sorted(sorted_result_list[i][1].columns))),
+            sorted_result_list[i][1],
+        ])
+    resultDF = pd.DataFrame(working_list, columns=[
+            'rank',
+            'RMSd',
+            'compounds',
+            'mutants',
+            'matrix'
+        ])
+    return resultDF
---
+++ run_OSF.py
@@ -0,0 +1,52 @@
+'''
+Runs OSF search in the terminal. Requires the orthogonal_set_finder.py file.
+'''
+from orthogonal_set_finder import *
+import argparse
+from datetime import datetime
+import pandas as pd
+from multiprocessing import Pool
+from itertools import repeat, chain
+
+def run_multiprocess(full_data, dimension, numProcesses=2):
+    '''
+    Method to run OSF search in multiple processes simultaneously.
+    '''
+    if __name__ == '__main__':
+        full_data = full_data.T # For algorithm to work, mutants must be in columns.
+        # [ lst[i::n] for i in xrange(n) ] Splits up lst into n segments
+        list_of_combinations = [ list(every_matrix(dimension,dimension,full_data))[i::numProcesses] for i in range(numProcesses) ]
+        pool = Pool(processes=numProcesses)
+        result_list = pool.starmap(iterate_RMSs, zip(list_of_combinations, repeat(full_data.copy())))
+        merged = list(chain.from_iterable(result_list))
+        return sorted(merged, key=lambda x: x[0])
+
+#################################################################
+# setup parser for accepting arguments from the bash shell
+parser = argparse.ArgumentParser(description='Multi-process(core) orthogonal set finder.')
+parser.add_argument('-i','--input', help='Input file name as .csv. Compounds in columns, mutants in rows.', required=True)
+parser.add_argument('-o','--output', help='Output file name as .csv', required=True)
+parser.add_argument('-d','--dimension', help='Dimension of search. Default: 2.', default=2, type=int)
+parser.add_argument('-p','--processes', help='Number of processes to spawn. Default: 1.', default=1, type=int)
+args = parser.parse_args()
+
+# run the script printing start and end times and the top five hits at the end.
+print('Running a {}x{} matrix search on {} with {} process(es).'.format(args.dimension, args.dimension, args.input, args.processes))
+try:
+    full_data = pd.read_csv(args.input, index_col=0, dtype='float64')
+except:
+    print("Something went wrong with the import of {}. Please check the file/path.".format(args.input))
+    raise
+starttime = datetime.now()
+print('Start time: {}'.format(starttime.isoformat()))
+result = run_multiprocess(full_data, args.dimension, numProcesses = args.processes)
+print("Done! Top five hits:")
+print(result[:5])
+endtime = datetime.now()
+print('End time: {}'.format(endtime.isoformat()))
+print('Total calculation time: {}'.format(str(endtime - starttime)))
+# May have to tweak the .to_csv() arguments to get rid of float formatting of
+# mutant numbers and extra quotes that appear around compounds.
+format_OSF(result).to_csv(args.output, index=False) # Write out result.
+print('Result saved to {}'.format(args.output))
+#################################################################
