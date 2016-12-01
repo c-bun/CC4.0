653e89a1d1e0a80b9031bad5b262484ac700cdac
c-bun <colrath@gmail.com>
2016-12-01 15:05:31 -0800

    increase sleep to 5

--- name_cores_dim_thr.sh
+++ name_cores_dim_thr.sh
@@ -24,6 +24,6 @@ python3 ~/data/CC4.0/run_OSF.py -i $jobname.csv -o $jobname_date -d $d -p $cores
 _EOF_
 
 qsub -N $jobname_date -q bio,pub64,free* -pe openmp $cores-$cores -m beas temp.sh
-sleep 2
+sleep 5
 rm temp.sh
 echo "---Done---"
