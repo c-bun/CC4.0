579cd4dc3bca6e3b5bf33d04f60b895b94df54a2
c-bun <colrath@gmail.com>
2016-12-01 15:01:32 -0800

    bugfix submission script

--- name_cores_dim_thr.sh
+++ name_cores_dim_thr.sh
@@ -12,10 +12,18 @@ echo "Name: $jobname_date"
 echo "Cores: $cores"
 echo "Dimensions: $d"
 echo "Threshold: $t"
-echo "full call: qsub -N $jobname_date -q bio,pub64,free* -pe openmp $cores-$cores -m beas \
-python3 ~/data/CC4.0/run_OSF.py -i $jobname.csv -o $jobname_date -d $d -p $cores -l 10000 -t $t \
+echo "full call: qsub -N $jobname_date -q bio,pub64,free* -pe openmp $cores-$cores -m beas"
+echo "python3 ~/data/CC4.0/run_OSF.py -i $jobname.csv -o $jobname_date -d $d -p $cores -l 10000 -t $t \
 > $jobname_date.log"
 
-qsub -N $jobname_date -q bio,pub64,free* -pe openmp $cores-$cores -m beas \
+cat << _EOF_ > temp.sh
+#!/bin/bash
+
 python3 ~/data/CC4.0/run_OSF.py -i $jobname.csv -o $jobname_date -d $d -p $cores -l 10000 -t $t \
 > $jobname_date.log
+_EOF_
+
+qsub -N $jobname_date -q bio,pub64,free* -pe openmp $cores-$cores -m beas temp.sh
+sleep 2
+rm temp.sh
+echo "---Done---"
