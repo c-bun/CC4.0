8d13a2332b388946daad6c360ec89ad8e5a181d6
Colin Rathbun <colrath@gmail.com>
2017-01-18 17:59:34 -0800

    Some improvements gained with .ix for indexing.

--- orthogonal_set_finder.py
+++ orthogonal_set_finder.py
@@ -11,32 +11,9 @@ set search to be used on the computational cluster or by other users.
 # imports
 import pandas as pd
 import numpy as np
-import matplotlib
-import itertools
-import threading
-from itertools import chain
-
-
-def norm_positives(l):
-    """
-    Helper function for the smooth_mat function to allow normalization of
-    only positive values.
-    """
-    p = l.copy()
-    p[p < 0] = 0
-    total = sum(p)
-    return l / total
-
-
-def smooth_mat(unmixed, remove_negatives=False):
-    """
-    Takes and unmixed array of correlations and normalizes rows(?) to one,
-    ignoring negative values. remove_negatives will zero the negative values
-    in the row(?).
-    """
-    if remove_negatives:
-        unmixed[unmixed < 0] = 0
-    return unmixed.apply(norm_positives, axis=1)
+from numpy import mean, sqrt, eye
+from numpy.linalg import norm
+from itertools import chain, repeat, combinations, product
 
 
 def clean_raw_data(pdarray):
@@ -49,28 +26,14 @@ def clean_raw_data(pdarray):
             # value in the array
 
 
-def trim_data(data, support):
-    """
-    Looks like this will trim a data set to only have the columns of a
-    second set. May still be useful...?
-    """
-    chosen = dict(zip(data.columns, support))
-    chosenList = []
-    for entry in chosen:
-        if chosen[entry] is True:
-            chosenList.append(entry)
-    newData = data[chosenList].copy()
-    return newData
-
-
 def every_matrix(m, n, pandasArray):
     """
     Accepts a pandas dataframe and returns an iterator with every possible
     combination of m rows and n columns via an iterator object.
     """
-    index_comb = itertools.combinations(pandasArray.index, m)
-    column_comb = itertools.combinations(pandasArray.columns, n)
-    index_column_prod = itertools.product(index_comb, column_comb)
+    index_comb = combinations(pandasArray.index, m)
+    column_comb = combinations(pandasArray.columns, n)
+    index_column_prod = product(index_comb, column_comb)
     return index_column_prod
 
 
@@ -79,60 +42,52 @@ def get_submatrix(full_data, combination_tuple):
     Accepts a tuple from the every_matrix() iterator to return the actual
     submatrix of the full data (not a copy).
     """
-    return full_data[
-        list(combination_tuple[1])].loc[list(combination_tuple[0])]
+    return full_data.ix[list(combination_tuple[0]), list(combination_tuple[1])]
+    # return full_data[
+    #    list(combination_tuple[1])].loc[list(combination_tuple[0])]
 
 
-def RMS_identity(pandasArray):
+def RMS_identity(pandasArray, identityMat):
     """
     Returns the average RMS error of the given matrix from the identity matrix.
     """
-    return np.sqrt(((
-        pandasArray - np.eye(pandasArray.shape[0])
+    return sqrt(((
+        pandasArray - identityMat
     ) ** 2).values.mean(axis=None))
-
-
-def avg_off_diag_value(npArray):
-    np.fill_diagonal(npArray, 0)
-    return npArray.mean(axis=None)
+    # square_distance = np.power((pandasArray - identityMat), 2)
+    # return np.sqrt(np.mean(square_distance.values))
 
 
 def normalize_vectors(pandasArray):
-    return pandasArray / np.linalg.norm(pandasArray, axis=0)
+    return pandasArray / norm(pandasArray, axis=0)
 
 
-def remove_dim_bands(full_data, threshold):
-    """
-    Still need to decide how to implement this. Would potentially filter
-    out low-emitting sets.
-    """
-    trimmed_dataframe = full_data.copy()
-    for column in full_data:
-        if np.max(full_data[column]) < threshold:
-            trimmed_dataframe.drop(column)
-    return trimmed_dataframe
-
-
-def check_RMSs(submatrix_indicies, full_data):
+def check_RMSs(submatrix_indicies, full_data, identityMat):
     '''
     Takes a tuple of the required indicies and the full matrix of data.
     Gets the rms and returns the RMS of the identity matrix as a scalar.
     '''
-    submatrix = get_submatrix(full_data, submatrix_indicies)
+    submatrix = get_submatrix(full_data, submatrix_indicies)  # TODO optimize
     submatrix_normd = normalize_vectors(submatrix)
     orthog_submatrix = submatrix_normd.dot(submatrix_normd.T)
-    result = RMS_identity(orthog_submatrix)
+    result = RMS_identity(orthog_submatrix, identityMat)  # TODO optimize
 
     return result
 
 
-def get_rms_from_combination(combination, full_data, threshold):
-    rms = check_RMSs(combination, full_data)
+def get_rms_from_combination(combination, full_data, threshold, identityMat):
+    rms = check_RMSs(combination, full_data, identityMat)
     if rms < threshold:
         return (rms, combination)
 
 
-def iterate_RMSs(list_to_process, full_data, threshold=1):
+def iterate_RMSs_map(list_to_process, full_data, identityMat, threshold=1):
+    result_list = map(get_rms_from_combination, list_to_process,
+                      repeat(full_data), repeat(threshold), repeat(identityMat))
+    return result_list
+
+
+def iterate_RMSs(list_to_process, full_data, identityMat, threshold=1):
     '''
     Takes a list of tuples of columns and rows to process and the full data
     matrix and iterates through the list, returning the RMS rating and the
@@ -140,28 +95,54 @@ def iterate_RMSs(list_to_process, full_data, threshold=1):
     within error of the screen.
     '''
     result_list = [get_rms_from_combination(
-        combination, full_data, threshold) for combination in list_to_process]
+        combination, full_data, threshold, identityMat) for combination in list_to_process]
+
+    return result_list
+
+
+def iterate_RMSs_old(list_to_process, full_data, identityMat, threshold=1):
+    '''
+    Takes a list of tuples of columns and rows to process and the full data
+    matrix and iterates through the list, returning the RMS rating and the
+    associated matrix. Specify a threshold of 0.15 to only get things that are
+    within error of the screen.
+    '''
+    result_list = []
+    for combination in list_to_process:
+        rms = check_RMSs(combination, full_data, identityMat)
+        if rms < threshold:
+            # try to reduce amount of memory used. this value is
+            # arbitrarily defined. RMSs of 0.15 seem to have resolutions that
+            # are ~ in error of current screen methodology. Might be
+            # good to have this as a parameter for the function in the future.
+            result_list.append((
+                rms,
+                combination
+            ))
 
     return result_list
 
 
 def o_score(rms, shape=(2, 2)):
     worst = pd.DataFrame(np.ones(shape))
-    worst_RMS = RMS_identity(worst)
+    worst_RMS = RMS_identity(worst, np.eye(shape[0]))
     return 2 * (worst_RMS / rms)
 
 
-def run_singleprocess(full_data, dimension, originalIteration=False):
+def run_singleprocess(full_data, dimension, iterationMethod='map'):
     '''
     Method to run OSF search in one processes for testing. Compounds must be in
     the rows of full_data for this method (not the case for run_multiprocess()
     in run_OSF.py.
     '''
     combinations = every_matrix(dimension, dimension, full_data)
-    if originalIteration:
-        # result_list =
-    else:
-        result_list = iterate_RMSs(combinations, full_data)
+    identityMat = np.eye(dimension)
+    if iterationMethod == 'for':
+        result_list = iterate_RMSs_old(combinations, full_data, identityMat)
+    elif iterationMethod == 'listComp':
+        result_list = iterate_RMSs(combinations, full_data, identityMat)
+    elif iterationMethod == 'map':
+        result_list = iterate_RMSs_map(combinations, full_data, identityMat)
     return sorted(result_list, key=lambda x: x[0])
 
 
