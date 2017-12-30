ca1475b873c428045cb74e2fff34eb81b82efb2f
Colin Rathbun <colrath@gmail.com>
2017-12-30 10:40:10 -0800

    Cleaned gui elements

--- gui.py
+++ gui.py
@@ -16,16 +16,19 @@ class Model():
 
     def __init__(self):
         self.df = None
-        self.resultDf = None
+        self.resultList = None
         self.dimension = (2, 2)
         self.processes = 2
         self.seq_addition = False
 
     def run_CC(self):
-        self.resultDf = run_multiprocess(
+        self.resultList = run_multiprocess(
             self.df, self.dimension, numProcesses=self.processes, seq_addition=self.seq_addition)
-        print('Done!: {}'.format(self.resultDf[:10]))
-        return self.resultDf
+        print('Done!: {}'.format(self.resultList[:10]))
+        return self.resultList
+
+    def format_result(self):
+        return format_OSF(self.resultList, self.df)
 
     def open_CSV(self, filename):
         try:
@@ -43,7 +46,8 @@ class View():
     def __init__(self, master):
         # Path box and load button frame
         loadFrame = Tk.Frame(master)
-        loadFrame.grid(row=0, columnspan=4)
+        #loadFrame.grid(row=0, columnspan=4)
+        loadFrame.pack()
         self.label = Tk.Label(
             loadFrame, text="Select a CSV file for processing:")
         self.label.pack(side=Tk.LEFT)
@@ -59,11 +63,13 @@ class View():
 
         # Area for printing input file contents
         self.dfBox = Tk.Text(master, relief=Tk.RIDGE, borderwidth=2)
-        self.dfBox.grid(columnspan=4, row=1)
+        #self.dfBox.grid(columnspan=4, row=1)
+        self.dfBox.pack()
 
         # Area for selecting options for the script run
         optionsFrame = Tk.Frame(master)
-        optionsFrame.grid(row=2, columnspan=4)
+        #optionsFrame.grid(row=2, columnspan=4)
+        optionsFrame.pack()
 
         dimensionLabel = Tk.Label(optionsFrame, text="Dimension:")
         dimensionLabel.pack(side=Tk.LEFT)
@@ -90,9 +96,11 @@ class View():
 
         # Area for printing result
         self.resultBox = Tk.Text(master, relief=Tk.RIDGE, borderwidth=2)
-        self.resultBox.grid(columnspan=4, row=3)
+        #self.resultBox.grid(columnspan=4, row=3)
+        self.resultBox.pack()
 
-        # Area for save and close buttons
+        # Area for save and close buttons TODO add a frame here to house these
+        # buttons
         self.save_button = Tk.Button(master, text="Save As")
         self.save_button.grid(row=4, column=1)
 
@@ -110,8 +118,6 @@ class Controller():
         self.view.load_button.bind('<Button>', self.loaddf)
         self.view.run_button.bind('<Button>', self.runOSF)
         self.view.save_button.bind('<Button>', self.saveResult)
-        # self.view.sidepanel.plotBut.bind("<Button>", self.my_plot)
-        # self.view.sidepanel.clearButton.bind("<Button>", self.clear)
 
     def loaddf(self, event):
         filename = Tk.filedialog.askopenfilename(
@@ -132,23 +138,24 @@ class Controller():
 
     def runOSF(self, event):
         if self.model.df is None:
-            # TODO this should be a dialog.
+            messagebox.showwarning(
+                "No data to run", "You must first load a CSV file")
             print("you must first load a CSV file.")
         else:
             result = self.model.run_CC()
             self.view.resultBox.delete("1.0", Tk.END)
+            # TODO Make this display a human-readable result.
             self.view.resultBox.insert(Tk.END, str(result[:10]))
 
     def saveResult(self, event):
-        if self.model.resultDf is None:
+        if self.model.resultList is None:
             messagebox.showwarning(
                 "No data to save.", "You must first run an analysis.")
         else:
             savePath = Tk.filedialog.asksaveasfilename(
                 initialdir='./', title='Select a save location.')
             try:
-                # FIXME resultDf is a list, not a pd df. Need to fix that.
-                self.model.resultDf.to_csv(savePath, index=False)
+                self.model.format_result().to_csv(savePath, index=False)
             except:
                 messagebox.showerror(
                     "Save file", "Something went wrong when saving {}.".format(savePath))
